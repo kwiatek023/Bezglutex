e475c688c5b82dd80302c958602965078f92be17
stokrotka24 <hanna.stempniewicz@gmail.com>
2021-01-08 11:09:04 +0100

    Update file to create database vol.2

--- sql/bezglutex.sql
+++ sql/bezglutex.sql
@@ -1,9 +1,6 @@
-DROP
-DATABASE IF EXISTS bezglutex;
-CREATE
-DATABASE bezglutex;
-USE
-bezglutex;
+DROP DATABASE IF EXISTS bezglutex;
+CREATE DATABASE bezglutex;
+USE bezglutex;
 
 CREATE TABLE customers
 (
@@ -17,7 +14,7 @@ CREATE TABLE customers
     postal_code VARCHAR(16)  NOT NULL,
     email       VARCHAR(120) NOT NULL,
 
-    CONSTRAINT customers_pk PRIMARY KEY (customer_id)
+    PRIMARY KEY (customer_id)
 );
 
 CREATE TABLE orders
@@ -28,20 +25,8 @@ CREATE TABLE orders
     payment     ENUM ('bank_transfer', 'on_delivery') NOT NULL,
     realized    BOOLEAN NOT NULL,
 
-    CONSTRAINT orders_pk PRIMARY KEY (order_id),
-    CONSTRAINT customers_fk FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
-);
-
-CREATE TABLE orders_products
-(
-    order_id   INT NOT NULL,
-    product_id INT NOT NULL,
-    quantity   INT NOT NULL,
-
-    CONSTRAINT orders_products_pk PRIMARY KEY (order_id, product_id),
-    CONSTRAINT orders_fk FOREIGN KEY (order_id) REFERENCES orders (order_id),
-    CONSTRAINT products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
-    CONSTRAINT nonnegative_quantity CHECK (quantity >= 0)
+    PRIMARY KEY (order_id),
+    CONSTRAINT orders_customers_fk FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
 );
 
 CREATE TABLE products
@@ -50,8 +35,20 @@ CREATE TABLE products
     type       ENUM ('breadstuff', 'pasta', 'dessert') NOT NULL,
     price      DECIMAL(6, 2) NOT NULL,
 
-    CONSTRAINT products_pk PRIMARY KEY (product_id),
+    PRIMARY KEY (product_id),
     CONSTRAINT nonnegative_price CHECK (price >= 0)
+);
+
+CREATE TABLE orders_products
+(
+    order_id   INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity   INT NOT NULL,
+
+    PRIMARY KEY (order_id, product_id),
+    CONSTRAINT orders_products_orders_fk FOREIGN KEY (order_id) REFERENCES orders (order_id),
+    CONSTRAINT orders_products_products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
+    CONSTRAINT nonnegative_quantity CHECK (quantity >= 0)
 );
 
 CREATE TABLE breadstuff
@@ -66,8 +63,8 @@ CREATE TABLE breadstuff
     pieces_per_package INT NOT NULL,
     energy_value       INT NOT NULL,
 
-    CONSTRAINT breadstuff_pk PRIMARY KEY (product_id),
-    CONSTRAINT products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
+    PRIMARY KEY (product_id),
+    CONSTRAINT breadstuff_products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
     CONSTRAINT nonnegative_netto_weight CHECK (netto_weight >= 0),
     CONSTRAINT nonnegative_pieces_per_package CHECK (pieces_per_package >= 0),
     CONSTRAINT nonnegative_energy_value CHECK (energy_value >= 0)
@@ -83,8 +80,8 @@ CREATE TABLE pasta
     energy_value INT NOT NULL,
     boil_time    INT NOT NULL,
 
-    CONSTRAINT pasta_pk PRIMARY KEY (product_id),
-    CONSTRAINT products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
+    PRIMARY KEY (product_id),
+    CONSTRAINT pasta_products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
     CONSTRAINT nonnegative_netto_weight CHECK (netto_weight >= 0),
     CONSTRAINT nonnegative_boil_time CHECK (boil_time >= 0),
     CONSTRAINT nonnegative_energy_value CHECK (energy_value >= 0)
@@ -98,8 +95,8 @@ CREATE TABLE desserts
     netto_weight INT         NOT NULL,
     energy_value INT         NOT NULL,
 
-    CONSTRAINT desserts_pk PRIMARY KEY (product_id),
-    CONSTRAINT products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
+    PRIMARY KEY (product_id),
+    CONSTRAINT desserts_products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
     CONSTRAINT nonnegative_netto_weight CHECK (netto_weight >= 0),
     CONSTRAINT nonnegative_energy_value CHECK (energy_value >= 0)
 );
@@ -109,8 +106,8 @@ CREATE TABLE storage
     product_id INT NOT NULL,
     quantity   INT NOT NULL,
 
-    CONSTRAINT storage_pk PRIMARY KEY (product_id),
-    CONSTRAINT products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
+    PRIMARY KEY (product_id),
+    CONSTRAINT storage_products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
     CONSTRAINT nonnegative_quantity CHECK (quantity >= 0)
 );
 
@@ -125,7 +122,7 @@ CREATE TABLE suppliers
     postal_code VARCHAR(16)  NOT NULL,
     email       VARCHAR(120) NOT NULL,
 
-    CONSTRAINT suppliers_pk PRIMARY KEY (supplier_id)
+    PRIMARY KEY (supplier_id)
 );
 
 
@@ -136,8 +133,8 @@ CREATE TABLE supplies
     date        DATE NOT NULL,
     payment     ENUM ('bank_transfer', 'on_delivery') NOT NULL,
 
-    CONSTRAINT supplies_pk PRIMARY KEY (supply_id),
-    CONSTRAINT suppliers_fk FOREIGN KEY (supplier_id) REFERENCES suppliers (supplier_id)
+    PRIMARY KEY (supply_id),
+    CONSTRAINT supplies_suppliers_fk FOREIGN KEY (supplier_id) REFERENCES suppliers (supplier_id)
 );
 
 CREATE TABLE supplies_products
@@ -146,9 +143,9 @@ CREATE TABLE supplies_products
     product_id INT NOT NULL,
     quantity   INT NOT NULL,
 
-    CONSTRAINT supplies_products_pk PRIMARY KEY (supply_id, product_id),
-    CONSTRAINT supplies_fk FOREIGN KEY (supply_id) REFERENCES supplies (supply_id),
-    CONSTRAINT products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
+    PRIMARY KEY (supply_id, product_id),
+    CONSTRAINT supplies_products_supplies_fk FOREIGN KEY (supply_id) REFERENCES supplies (supply_id),
+    CONSTRAINT supplies_products_products_fk FOREIGN KEY (product_id) REFERENCES products (product_id),
     CONSTRAINT nonnegative_quantity CHECK (quantity >= 0)
 );
 
@@ -159,5 +156,5 @@ CREATE TABLE users
     password VARCHAR(20) NOT NULL,
     type     ENUM ('sales_man', 'admin', 'store_keeper', 'store_manager') NOT NULL,
 
-    CONSTRAINT users_pk PRIMARY KEY (user_id)
+    PRIMARY KEY (user_id)
 );
