4fc484e66c7f95e77e9171fe5b02cc80ff5fa51d
stokrotka24 <hanna.stempniewicz@gmail.com>
2021-01-15 23:04:14 +0100

    add breadstuffview

--- src/main/java/layout/controllers/AddBreadstuffController.java
+++ src/main/java/layout/controllers/AddBreadstuffController.java
@@ -1,10 +1,12 @@
 package layout.controllers;
 
+import database.BreadstuffType;
 import database.connection.SessionManager;
 import database.entities.BreadstuffEntity;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -12,9 +14,12 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.stage.Window;
+import layout.exceptions.FailedAddingSupplierException;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureQuery;
 import java.math.BigDecimal;
 import java.util.Optional;
 
@@ -22,7 +27,6 @@ public class AddBreadstuffController {
     private int supplyId;
     private SessionManager sessionManager;
     private final ObservableList<BreadstuffEntity> breadstuffEntities = FXCollections.observableArrayList();
-
 
     @FXML
     public TableColumn<BreadstuffEntity, Integer> productId;
@@ -46,7 +50,7 @@ public class AddBreadstuffController {
     public TableView<BreadstuffEntity> tableView;
 
     @FXML
-    public TextField typeTextField;
+    public ComboBox<BreadstuffType> typeBox;
 
     @FXML
     public TextField nettoWeightTextField;
@@ -61,26 +65,27 @@ public class AddBreadstuffController {
     public TextField priceTextField;
 
     @FXML
-    public TextField quantityTextField;
-
-    @FXML
     public void initialize() {
         this.sessionManager = SessionManager.getInstance();
         createTableView();
+        typeBox.setItems(FXCollections.observableArrayList(BreadstuffType.values()));
     }
 
 
     public void createTableView() {
         tableView.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
             if (newValue != null) {
-                addBreadstuff(newValue);
+                Integer quantity = showSelectQuantityDialog();
+                if(quantity != null) {
+                    addBreadstuff(newValue, quantity);
+                    displayInfoAlert("Products successfully added to new supply");
+                }
             }
         });
 
-        Session session = sessionManager.openSession();
+        Session session = sessionManager.getCurrentSession();
         Query<BreadstuffEntity> query = session.createQuery("FROM BreadstuffEntity", BreadstuffEntity.class);
         breadstuffEntities.addAll(query.getResultList());
-        sessionManager.closeSession();
 
         tableView.setItems(breadstuffEntities);
 
@@ -92,7 +97,7 @@ public class AddBreadstuffController {
         price.setCellValueFactory(cellData -> new SimpleObjectProperty<>(cellData.getValue().getProductsEntity().getPrice()));
     }
 
-    private void addBreadstuff(BreadstuffEntity breadstuff) {
+    private Integer showSelectQuantityDialog() {
         Dialog<ButtonType> dialog = new Dialog<>();
 
         dialog.setTitle("Quantity");
@@ -116,15 +121,91 @@ public class AddBreadstuffController {
 
         Optional<ButtonType> result = dialog.showAndWait();
         if (result.isPresent() && result.get() == ButtonType.OK) {
-//            if(addProductsController.supplyIsEmpty()) {
-//                return ControllerCommunicator.getInstance().getMsg();
-//            }
+            try {
+                int quantity = Integer.parseInt(textField.getText());
+
+                if (quantity >= 0) {
+                    return quantity;
+                }
+                displayErrorAlert("Quantity must be positive!");
+            } catch (NumberFormatException ex) {
+                displayErrorAlert("Incorrect number.");
+            }
+            return showSelectQuantityDialog();
         }
 
-//        return null;
+        return null;
+    }
+
+    private void addBreadstuff(BreadstuffEntity breadstuff, Integer quantity) {
+        StoredProcedureQuery procedureQuery = sessionManager.getCurrentSession().createStoredProcedureQuery("add_breadstuff").
+                registerStoredProcedureParameter("_type", String.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("_netto_weight", Integer.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("_pieces_per_package", Integer.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("_energy_value", Integer.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("_price", BigDecimal.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("_supply_id", Integer.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("_quantity", Integer.class, ParameterMode.IN)
+                .setParameter("_type", breadstuff.getType().toString())
+                .setParameter("_netto_weight", breadstuff.getNettoWeight())
+                .setParameter("_pieces_per_package", breadstuff.getPiecesPerPackage())
+                .setParameter("_energy_value", breadstuff.getEnergyValue())
+                .setParameter("_price", breadstuff.getProductsEntity().getPrice())
+                .setParameter("_supply_id", supplyId)
+                .setParameter("_quantity", quantity);
+        procedureQuery.execute();
     }
 
     public void setSupplyId(int supplyId) {
         this.supplyId = supplyId;
+    }
+
+    private void displayInfoAlert(String contextText) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(null);
+        alert.setHeaderText(null);
+        alert.setContentText(contextText);
+        alert.showAndWait();
+    }
+
+    private void displayErrorAlert(String contextText) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(null);
+        alert.setHeaderText(null);
+        alert.setContentText(contextText);
+        alert.showAndWait();
+    }
+
+    public void addNewBreadstuffClicked(ActionEvent actionEvent) {
+        boolean allDataFilled = typeBox.getSelectionModel().getSelectedItem() != null &&
+                !nettoWeight.getText().isBlank() && !piecesPerPackage.getText().isBlank() &&
+                !energyValue.getText().isBlank() && !price.getText().isBlank();
+
+        if (allDataFilled) {
+            try {
+                BreadstuffEntity breadstuff =  new BreadstuffEntity();
+                breadstuff.setType(typeBox.getSelectionModel().getSelectedItem());
+                breadstuff.set
+                addBreadstuff(typeBox.getSelectionModel().getSelectedItem(), nettoWeight.getText(), piecesPerPackage.getText(),
+                        energyValue.getText(), price.getText(), streetTextField.getText(), postalCodeTextField.getText());
+                return true;
+            } catch(FailedAddingSupplierException failedAddingSupplierException) {
+                displayErrorAlert("This supplier exists.");
+            } catch (Exception ex) {
+                displayErrorAlert("Operation failed");
+            }
+        } else {
+            displayErrorAlert("Fill all data to add supplier.");
+        }
+
+        nameTextField.setText("");
+        nipTextField.setText("");
+        emailTextField.setText("");
+        countryTextField.setText("");
+        cityTextField.setText("");
+        streetTextField.setText("");
+        postalCodeTextField.setText("");
+
+        return false;
     }
 }
